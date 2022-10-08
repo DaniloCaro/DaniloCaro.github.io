b8dd8efe87adf28564e8183bcf8457c6d153b959
Danilo Caro <danilocaro23@gmail.com>
2022-10-07 21:41:45 -0500

    Use projects from service

--- src/app/website/services/projects.service.ts
+++ src/app/website/services/projects.service.ts
@@ -1,34 +1,118 @@
-import { ProjectInterface } from './../interface/project.interface';
 import { ProjectApiInterface } from './../interface/project-api.interface';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { ProjectInterface } from './../interface/project.interface';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
   // URL de la API
-  private myAppUrl:string = 'https://portafolio-danilo.herokuapp.com/';
-  private myApiProjects:string = 'api/project/';
+  // private myAppUrl:string = 'https://portafolio-danilo.herokuapp.com/';
+  // private myApiProjects:string = 'api/project/';
   // Objeto de prueba
-  private project: ProjectInterface[] = [
+  private project: ProjectApiInterface[] = [
     {
-      name:'Prueba técnica frontend developer Angular',
-      description: 'Desarrollo de una prueba técnica para una empresa de desarrollo de software en Angular utilizando un prototipo en Adobe XD para resolución de pantalla 1920x1080. \nUser: eve.holt@reqres.in Password: cityslicka.',
-      skills: 'Angular, API Rest, JWT, Nodejs, Typescript, HTML, CSS, npm.',
-      link: 'https://petcare-a5766.web.app/',
-      img:'./assets/img/petcare/petcare(max-600).webp',
+        "id": 3,
+        "img": "./assets/img/CreditCards-Angular.webp",
+        "title": "CreditCards",
+        "description": "CRUD sobre datos de tarjetas de credito sin uso de Database.",
+        "skills": [
+            "Bootstrap, HTML, CSS, TypeScript, Angular."
+        ],
+        "link": "https://creditcards-angular.netlify.app/"
+    },
+    {
+        "id": 4,
+        "img": "./assets/img/Chatboa.webp",
+        "title": "ChatBoa App Web",
+        "description": "Sala de chat funcional.",
+        "skills": [
+            "Bootstrap, HTML, CSS, JavaScript, NodeJS."
+        ],
+        "link": "https://chatboa.herokuapp.com/"
+    },
+    {
+        "id": 5,
+        "img": "./assets/img/Platzi-Conf.webp",
+        "title": "Platzi Conf. Clon",
+        "description": "Maquetación del sitio web de Platzi Conf.",
+        "skills": [
+            "Bootstrap, HTML, CSS, JavaScript."
+        ],
+        "link": "https://platziconference.netlify.app/"
+    },
+    {
+        "id": 6,
+        "img": "./assets/img/Batatabit.webp",
+        "title": "Batatabit Landing Page",
+        "description": "Landing page de empresa Batatabit utilizando la metodología Mobile First.",
+        "skills": [
+            "BEM, HTML, CSS"
+        ],
+        "link": "https://github.com/DaniloCaro/MobileFirst"
+    },
+    {
+        "id": 7,
+        "img": "./assets/img/CRUD-Estudiante.webp",
+        "title": "Consumo de API Estudiantes",
+        "description": "CRUD sobre datos basicos de estudiantes.",
+        "skills": [
+            "Bootstrap",
+            "HTML",
+            "CSS",
+            "TypeScript",
+            "Angular",
+            "NodeJS"
+        ],
+        "link": "https://seminario-crud.netlify.app/"
+    },
+    {
+        "id": 8,
+        "img": "./assets/img/petcare/petcare(max-600).webp",
+        "title": "Prueba técnica frontend developer Angular",
+        "description": "Desarrollo de una prueba técnica para una empresa de desarrollo de software en Angular utilizando un prototipo en Adobe XD para resolución de pantalla 1920x1080. \nUser: eve.holt@reqres.in Password: cityslicka.",
+        "skills": [
+            "Angular",
+            "API Rest",
+            "JWT",
+            "Nodejs",
+            "Typescript",
+            "npm"
+        ],
+        "link": "https://petcare-a5766.web.app/"
+    },
+    {
+        "id": 2,
+        "img": "./assets/img/Cosmos.webp",
+        "title": "Cosmos App Web",
+        "description": "Aplicación web para la recepción sistemática de información requerida en matriculas académicas.",
+        "skills": [
+            "CSS, HTML, JS, Docker,Python (Flask), PostgreSql."
+        ],
+        "link": "https://cosmosudc.herokuapp.com/"
+    },
+    {
+        "id": 1,
+        "img": "./assets/img/servicer.webp",
+        "title": "Landing page Servicer Ltda.",
+        "description": "Landing page para empresa de soluciones integrales.",
+        "skills": [
+            "CSS, HTML, JS, Bootstrap."
+        ],
+        "link": "https://servicer-lp.herokuapp.com/"
     }
-  ]
-  constructor(private http: HttpClient) {
-  }
+]
+  // constructor(private http: HttpClient) {
+  // }
+  constructor() {}
   // Obtiene los proyectos de la API
-  getProjectsApi() : Observable<ProjectApiInterface[]> {
-    return this.http.get<ProjectApiInterface[]>(this.myAppUrl + this.myApiProjects);
-  }
+  // getProjectsApi() : Observable<ProjectApiInterface[]> {
+  //   return this.http.get<ProjectApiInterface[]>(this.myAppUrl + this.myApiProjects);
+  // }
   // Obtiene los proyectos de prueba
-  getProjects(): ProjectInterface[] {
+  getProjects(): ProjectApiInterface[] {
     return this.project;
   }
 }
--- src/app/website/templates/myprojects/myprojects.component.ts
+++ src/app/website/templates/myprojects/myprojects.component.ts
@@ -15,27 +15,25 @@ export class MyprojectsComponent implements OnInit {
   constructor(private _projectsService:ProjectsService) { }
 
   // Objeto de prueba
-  projects:ProjectInterface[]=[]
+  projects:ProjectApiInterface[]=[]
   // Objeto de la API
   projectsApi:ProjectApiInterface[]=[]
 
   ngOnInit(): void {
     // Objeto de prueba
     this.projects = this._projectsService.getProjects();
+    // Ordenar el objeto de prueba por ID en forma descendente
+    this.projects.sort((a,b)=>b.id-a.id);
     // Objeto de la API
-    // this._projectsService.getProjectsApi().subscribe(data => {
-    //   const descData = data.sort((a, b) => (a > b) ? 1 : -1);// Ordena los proyectos de la API por fecha de creación
-    //   this.projectsApi = descData;
+    // this._projectsService.getProjectsApi().subscribe({
+    //   next: (data) => {
+    //     // Ordena los proyectos de la API por id en orden descendente
+    //     const descData = data.sort((a, b) => (a.id > b.id) ? -1 : 1);
+    //     this.projectsApi = descData;
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //   }
     // });
-    this._projectsService.getProjectsApi().subscribe({
-      next: (data) => {
-        // Ordena los proyectos de la API por id en orden descendente
-        const descData = data.sort((a, b) => (a.id > b.id) ? -1 : 1);
-        this.projectsApi = descData;
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
   }
 }
