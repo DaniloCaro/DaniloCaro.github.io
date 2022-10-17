16e9d5c1531b9fea2a4a851bd45692c1229f65dc
Danilo Caro <danilocaro23@gmail.com>
2022-10-16 21:21:42 -0500

    Implement Onload()

--- src/app/website/pages/project-page/project-page.component.ts
+++ src/app/website/pages/project-page/project-page.component.ts
@@ -14,16 +14,24 @@ export class ProjectPageComponent implements OnInit {
 
   titleProject!: string;
   projectOnly!:ProjectApiInterface[];
-  // public loading: boolean = true;
+  public loading!: boolean;
 
   constructor(private activatedRoute: ActivatedRoute, private _projectsService: ProjectsService,private router: Router) {
     this.activatedRoute.params.subscribe(params => {
-      // this.loading = true;
       this.titleProject = params['title'];
 
       this._projectsService.getProjectObservable(this.titleProject).subscribe({
         next:(data:ProjectApiInterface[])=>{
           this.projectOnly = data;
+          this.loading = true;
+          // Cambiar loading a false mientras se carga la imagen
+          if (this.projectOnly[0].img) {
+            const img = new Image();
+            img.onload = () => {
+              this.loading = false;
+            }
+            img.src = this.projectOnly[0].img;
+          }
           if (this.projectOnly == undefined || this.projectOnly.length == 0) {
             this.router.navigate(['/', 'not-found']);
           }
